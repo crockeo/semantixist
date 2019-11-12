089aa7b14b85af9f7fe8c4e67393f75075b31e33
Cerek Hillen <cerekh@gmail.com>
2019-11-12 10:52:55 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,119 @@
+from enum import Enum
+
+
+class Function:
+    """
+    Basic implemenetation of a lambda function
+    """
+
+    def __init__(self, variable, tokens):
+        self.variable = variable
+        if isinstance(tokens, str):
+            self.tokens = tokens.split()
+        else:
+            self.tokens = tokens
+
+    def __str__(self):
+        return '[{}{}. {}]'.format(
+            'λ',
+            self.variable,
+            ' '.join([str(token) for token in self.tokens]),
+        )
+
+    def __repr__(self):
+        return str(self)
+
+    def replace(self, variable, value):
+        tokens = []
+        for token in self.tokens:
+            if isinstance(token, str):
+                if token == variable:
+                    tokens.append(value)
+                else:
+                    tokens.append(token)
+            else:
+                tokens.append(token.replace(variable, value))
+
+        return Function(self.variable, tokens)
+
+    def apply(self, value):
+        tokens = self.replace(self.variable, value).tokens
+        if len(tokens) == 1:
+            return tokens[0]
+        else:
+            return tokens
+
+
+class Entity:
+    """
+    Explicit Entity type, so that we can separate them from variable holes.
+    """
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return str(self)
+
+    def replace(self, variable, value):
+        return self
+
+
+class Type:
+    """
+    Wraps around types to explicate the kinds of things members of a semantic tree can be.
+    """
+
+    def __init__(self, from_type, to_type=None):
+        self.from_type = from_type
+        self.to_type = to_type
+
+    def __eq__(self, other):
+        if not isinstance(other, Type):
+            return False
+        return self.from_type == other.from_type and self.to_type == other.to_type
+
+    def __str__(self):
+        if self.to_type is None:
+            return str(self.from_type)
+        else:
+            return '<{}, {}>'.format(self.from_type, self.to_type)
+
+    def __repr__(self):
+        return str(self)
+
+
+class CompositionRule(Enum):
+    """
+    The set of rules by which we compose trees into their final truth conditions.
+    """
+
+    FunctionApplication = 0
+
+
+def infer_composition_rule(type1: Type, type2: Type) -> CompositionRule:
+    """
+    Infers the way in which we want to compose nodes in our semantic tree based on their types.
+    """
+
+    if type1.from_type == type2 or type2.from_type == type1:
+        return CompositionRule.FunctionApplication
+
+mary = Entity('Mary')
+john = Entity('John')
+
+a = Function('x', [Function('y', 'y likes x')])
+
+print(a.apply(mary).apply(john))
+print(CompositionRule.FunctionApplication)
+print(Type('e'), Type('e', 't'))
+
+print(
+    infer_composition_rule(
+        Type(Type('e'), Type('t')),
+        Type('e'),
+    ),
+)
